f6292159ee99a32ca96aa5629c96d68f88ced656
Tobias <tobias.ljungstrom@gmail.com>
2019-01-24 13:21:05 +0100

    Add colors from design system

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import { Dashboard } from './components/dashboard/Dashboard';
 import styles from './App.module.css';
 import './normalize.css';
+import './colors.css';
 
 class App extends Component {
   render() {
     return (
       <div className={styles.app}>
-        <header>
+        <header className={styles.header}>
           <h1>Octopus</h1>
         </header>
 
@@ -15,7 +16,7 @@ class App extends Component {
           <Dashboard/>
         </main>
 
-        <footer>
+        <footer className={styles.footer}>
           <span>footer</span>
         </footer>
       </div>
