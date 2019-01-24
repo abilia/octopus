9912d0da3dea63633897ed7a81823d490c66d8d5
Tobias <tobias.ljungstrom@gmail.com>
2019-01-24 15:11:48 +0100

    Dynamically place and size squids

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,8 +1,35 @@
-import React, { Component } from 'react';
-import styles from './Dashboard.module.css';
+import React, { Component } from 'react'
+import styles from './Dashboard.module.css'
 
 export class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squids: [
+        {
+          name: 'Squid 1',
+          colStart: 7,
+          colEnd: 20,
+          rowStart: 3,
+          rowEnd: 21
+        }
+      ]
+    }
+  }
+  getStyle(squid) {
+    return {
+      gridColumnStart: squid.colStart,
+      gridColumnEnd: squid.colEnd,
+      gridRowStart: squid.rowStart,
+      gridRowEnd: squid.rowEnd
+    }
+  }
+
   render() {
-    return <div className={styles.dashboard}>Dis is dashboard</div>
+    return (
+      <div className={styles.dashboard}>
+        {this.state.squids.map(squid => <div className={styles.squid} style={this.getStyle(squid)}/>)}
+      </div>
+    )
   }
 }
