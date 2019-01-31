b34d5fff9c39de20c5615965b5a8c5b8ae70a7b5
Tobias Ljungström <tobias.ljungstrom@gmail.com>
2019-01-31 16:19:19 +0100

    Simplify bible word component

--- src/components/RssFeedSquid/BibleWordOfTodaySquid.js
+++ src/components/RssFeedSquid/BibleWordOfTodaySquid.js
@@ -1,14 +1,10 @@
 import React from 'react'
 
-import styles from './BibleWordOfTodaySquid.module.css'
-
 export function BibleWordOfTodaySquid(props) {
-    return (
-        <div className={styles.bibleWordOfToday}>
-            <div>
-                <h4>{props.bibleWordOfToday ? props.bibleWordOfToday.title : ""}</h4>
-                <p dangerouslySetInnerHTML={{ __html: props.bibleWordOfToday ? props.bibleWordOfToday.description : ""}}></p>
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <h4>{props.title}</h4>
+      <p>{props.text}</p>
+    </div>
+  )
 }
