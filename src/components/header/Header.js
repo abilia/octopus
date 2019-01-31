be0954254bcd9db6955cb79c663dc25f2d683219
Tobias Ljungström <tobias.ljungstrom@gmail.com>
2019-01-31 17:36:06 +0100

    Make the octopus accessible

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,7 +5,7 @@ export class Header extends Component {
   render() {
     return (
       <header className={styles.header}>
-        <h1 className={styles.title}>Octopus 🐙</h1>
+        <h1 className={styles.title}>Octopus <span role="img" aria-label="octopus">🐙</span></h1>
       </header>
     )
   }
