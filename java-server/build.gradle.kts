e2e30eb75eb3ef2962d97c60e06fb5d01cd1d9db
Johanna Lynn Lahti <johanna.lynn.lahti@abilia.se>
2020-09-28 08:54:41 +0200

    Add netflix-zuul to test routing

--- java-server/build.gradle.kts
+++ java-server/build.gradle.kts
@@ -17,10 +17,14 @@ repositories {
 }
 
 dependencies {
+    // compile("org.springframework.cloud:spring-cloud-starter-zuul")
+
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-zuul:2.1.3.RELEASE")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
